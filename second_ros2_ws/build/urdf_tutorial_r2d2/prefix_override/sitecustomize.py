e10a825dc0fc7437c37e844a06c58fac7c557820
Syed Razwanul Haque Nabil <nabilsust@gmail.com>
2023-08-09 12:22:15 +0600

    second_ros2_ws

---
+++ second_ros2_ws/build/urdf_tutorial_r2d2/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/nabil/second_ros2_ws/install/urdf_tutorial_r2d2'
---
+++ second_ros2_ws/build/urdf_tutorial_r2d2/share/urdf_tutorial_r2d2/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/nabil/second_ros2_ws/build/urdf_tutorial_r2d2"
