2ff742e9b1a48b0a14ed9be0ec85abc7f406231f
Syed Razwanul Haque Nabil <nabilsust@gmail.com>
2023-09-18 10:39:27 +0600

    Delete second_ros2_ws directory

--- second_ros2_ws/build/urdf_tutorial_r2d2/share/urdf_tutorial_r2d2/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/nabil/second_ros2_ws/build/urdf_tutorial_r2d2"
--- second_ros2_ws/build/urdf_tutorial_r2d2/share/urdf_tutorial_r2d2/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/nabil/second_ros2_ws/build/urdf_tutorial_r2d2"
